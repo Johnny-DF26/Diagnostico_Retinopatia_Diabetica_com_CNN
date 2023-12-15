3fd8fdb8a9b82c2ceb5e47e1d161a82d5810e0b1
Johnny <101834686+Johnny-DF26@users.noreply.github.com>
2023-12-15 13:39:29 -0300

    .update

--- src/app/main.py
+++ src/app/main.py
@@ -4,8 +4,8 @@ import cv2
 import matplotlib.pyplot as plt
 from keras.models import model_from_json
 import tensorflow as tf
-import os
-
+from PIL import Image
+from keras.preprocessing import image
 
 # Obtém o caminho absoluto para o arquivo JSON
 file_path_json = 'src/models/model_093/classificador_093.json'
@@ -27,18 +27,19 @@ st.write('Faça o upload de uma imagem para prever o diagnóstico')
 img_input = st.file_uploader("Escolha uma imagem", type=['jpg', 'jpeg'], accept_multiple_files=False)
 
 if img_input is not None:
-    file_bytes = img_input.read()
-    np_img = np.frombuffer(file_bytes, np.uint8)
-    img_orig = cv2.imdecode(np_img, cv2.IMREAD_COLOR)
-    img_orig = cv2.cvtColor(img_orig, cv2.COLOR_BGR2RGB)
-    img_cp = img_orig.copy()
 
-    img = img_orig / 255
-    img = cv2.resize(img, (224,224))
-    img = np.expand_dims(img, 0)
+    img = Image.open(img_input)
+    st.image(img, caption='Imagem carregada', use_column_width=True)
+
+    # Pré-processamento da imagem para fazer a previsão
+    img = img.resize((224, 224))  # Redimensionar a imagem para o tamanho esperado pelo modelo
+    img_cp = img.copy()
+    img = image.img_to_array(img)
+    img = img / 255
+    img = img.reshape((1, img.shape[0], img.shape[1], img.shape[2]))
 
     st.write("Imagem Enviada:")
-    st.image(img_orig, caption='Imagem enviada', use_column_width=False, width=200)
+    st.image(img, caption='Imagem enviada', use_column_width=False, width=200)
 
     # Realizar a previsão
     # st.write("Fazendo previsão...")
@@ -46,18 +47,15 @@ if img_input is not None:
     prediction_pct = model.predict(img)
     prediction = np.argmax(prediction_pct, axis=1)[0]
 
-
     # Exibir a imagem editada com o diagnóstico sobreposto
     st.write("Imagem com Diagnóstico:")
     
     # Lógica para interpretar a previsão e exibir o diagnóstico
     if prediction == 0:  
-        cv2.putText(img_cp, 'DR: Positive', (140,220), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0,255,0), 1)
         st.image(img_cp, caption='Diagnóstico sobreposto', use_column_width=False, width=500)
         st.write("Diagnóstico:")
         st.markdown(f'Paciente com<span style="color: red;"> Retinopatia Diabética</span> com {(prediction_pct[0][prediction]):.2%}.', unsafe_allow_html=True)
     else:
-        cv2.putText(img_cp, 'DR: Negative', (140,220), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0,255,0), 1)
         st.image(img_cp, caption='Diagnóstico sobreposto', use_column_width=False, width=500)
         st.write("Diagnóstico:")
         st.markdown(f'Paciente<span style="color: blue;"> Normal</span> com {(prediction_pct[0][prediction]):.2%}.', unsafe_allow_html=True)
